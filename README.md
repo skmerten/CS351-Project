c1c369fe4dff94cbc0abe6221ebeb4b5424938fe
Stephen Merten <skmerten@gmail.com>
2021-02-04 20:45:27 -0800

    Added code to scrape Monster for software engineer data

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CS351-Project
+
+requirements:
+
+pip install xlwt
+pip install requests
+pip install beautifulsoup4
--- Scrapper.py
+++ Scrapper.py
@@ -1,14 +1,8 @@
 # Package Import
 import requests
+from bs4 import BeautifulSoup
 # Excel Drivers
-import xlwt
-from xlwt import Workbook
-
-# Create Excel workbook to log locations with miatas
-wb = Workbook()
-sheet1 = wb.add_sheet('Data')
-    # Set row counter
-row = 0
+import csv
 
 # URL Data
 jobKeyword = 'Software Engineer'
@@ -22,5 +16,25 @@ URL = URL_BASE + URL_CRITERIA
 # GET URL
 page = requests.get(URL)
 
+soup = BeautifulSoup(page.content, 'html.parser')
+results = soup.find(id='ResultsContainer')
 
-wb.save('Scrapped Data.xls')
+
+job_elems = results.find_all('section', class_='card-content')
+
+with open('Scrapper_Data.csv', 'w', newline='') as csvfile:
+    #dataWriter = csv.writer(csvfile, delimiter= ' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    dataWriter = csv.writer(csvfile, delimiter= ',')
+    for job_elem in job_elems:
+        # Each job_elem is a new BeautifulSoup object.
+        # You can use the same methods on it as you did before.
+        title_elem = job_elem.find('h2', class_='title')
+        company_elem = job_elem.find('div', class_='company')
+        location_elem = job_elem.find('div', class_='location')
+        if None in (title_elem, company_elem, location_elem):
+            continue
+        title = title_elem.text.strip()
+        company = company_elem.text.strip()
+        location = location_elem.text.strip()
+
+        dataWriter.writerow([title, company, location])
