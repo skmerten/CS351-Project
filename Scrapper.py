97f019c353f286f7a2586c4aa0ab1297f50e3adc
Stephen Merten <skmerten@gmail.com>
2021-02-06 09:41:23 -0800

    Added the Regex to extract lising post date

--- Scrapper.py
+++ Scrapper.py
@@ -1,4 +1,5 @@
 # Package Import
+import re
 import requests
 from bs4 import BeautifulSoup
 # Excel Drivers
@@ -8,30 +9,38 @@ import csv
 jobKeyword = 'Software Engineer'
 jobLocation = 'San Diego'
 
+# URL Construct
 URL_BASE = 'https://www.monster.com/jobs/search/?q='
 URL_CRITERIA = jobKeyword + '&where=' + jobLocation
 URL = URL_BASE + URL_CRITERIA
 
 
-# GET URL
+# GET Webpage at URL
 page = requests.get(URL)
 
+# Parse into more manageable content
 parsedContennt = BeautifulSoup(page.content, 'html.parser')
+
+# Filter content for Job Listings
 jobResults = parsedContennt.find(id='ResultsContainer')
-
-
 jobs = jobResults.find_all('section', class_='card-content')
 
+# Open CSV file for input
 with open('Scrapper_Data.csv', 'w', newline='') as csvfile:
     dataWriter = csv.writer(csvfile, delimiter= ',')
-    for jobs in jobss:
-        jobTitle = jobs.find('h2', class_='title')
-        jobCompany = jobs.find('div', class_='company')
-        jobLocation = jobs.find('div', class_='location')
+
+    #Loop through each job entry
+    for job in jobs:
+        # Locate Job's Title
+        jobTitle = job.find('h2', class_='title')
+        jobCompany = job.find('div', class_='company')
+        jobLocation = job.find('div', class_='location')
+        jobPostDate = str(job.find('time'))
+        date = re.search(r'(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})', jobPostDate)
         if None in (jobTitle, jobCompany, jobLocation):
             continue
         title = jobTitle.text.strip()
         company = jobCompany.text.strip()
         location = jobLocation.text.strip()
 
-        dataWriter.writerow([title, company, location])
+        dataWriter.writerow([title, company, location, date])
