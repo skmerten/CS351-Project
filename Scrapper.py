2fdb459f7c47f879c7e07afb6417e0d86ac98b9d
Stephen Merten <skmerten@gmail.com>
2021-02-06 08:48:33 -0800

    Updated Scrapper file

--- Scrapper.py
+++ Scrapper.py
@@ -16,25 +16,22 @@ URL = URL_BASE + URL_CRITERIA
 # GET URL
 page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='ResultsContainer')
+parsedContennt = BeautifulSoup(page.content, 'html.parser')
+jobResults = parsedContennt.find(id='ResultsContainer')
 
 
-job_elems = results.find_all('section', class_='card-content')
+jobs = jobResults.find_all('section', class_='card-content')
 
 with open('Scrapper_Data.csv', 'w', newline='') as csvfile:
-    #dataWriter = csv.writer(csvfile, delimiter= ' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     dataWriter = csv.writer(csvfile, delimiter= ',')
-    for job_elem in job_elems:
-        # Each job_elem is a new BeautifulSoup object.
-        # You can use the same methods on it as you did before.
-        title_elem = job_elem.find('h2', class_='title')
-        company_elem = job_elem.find('div', class_='company')
-        location_elem = job_elem.find('div', class_='location')
-        if None in (title_elem, company_elem, location_elem):
+    for jobs in jobss:
+        jobTitle = jobs.find('h2', class_='title')
+        jobCompany = jobs.find('div', class_='company')
+        jobLocation = jobs.find('div', class_='location')
+        if None in (jobTitle, jobCompany, jobLocation):
             continue
-        title = title_elem.text.strip()
-        company = company_elem.text.strip()
-        location = location_elem.text.strip()
+        title = jobTitle.text.strip()
+        company = jobCompany.text.strip()
+        location = jobLocation.text.strip()
 
         dataWriter.writerow([title, company, location])
