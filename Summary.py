ce29ff5e9019ddf05f524858525ef75fed88113f
Stephen Merten <skmerten@gmail.com>
2021-02-13 13:25:00 -0800

    WIP - Trying to get the Legend and the pie chart. No bar graph

--- Summary.py
+++ Summary.py
@@ -1,22 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 import csv
 
 from random import random
-
+'''
 def draw_bar_graph(progLang: np.ndarray,
                    frequencyArr: np.ndarray):
-    '''
+    
     Draws the bar graph visual, note that it does NOT display a figure. You can
        add the "show()" method to a desired line for display.
-    '''
+    
     if len(progLang) is not len(frequencyArr):
         raise Exception()
 
     for p, f in zip(progLang, frequencyArr):
         rgb = [random(), random(), random()]
         plt.bar(p, f, color=rgb)
+'''
+
 
 
 def draw_pie_charts(progLang: np.ndarray,
@@ -28,9 +29,12 @@ def draw_pie_charts(progLang: np.ndarray,
     patches,_ = plt.pie(frequencyArr, startangle=90, shadow=True, radius=1.3)
     porcent = 100*frequencyArr/frequencyArr.sum()
     labels = ['{0} - {1:1.2f} %'.format(i,j) for i,j in zip(progLang, porcent)]
-
+    return patches, labels
+    
+def draw_pie_chart_legend(patches, labels):
     plt.legend(patches, labels, loc='center',fontsize=8,
-               bbox_to_anchor=(0.15, 0.,0.65, -0.5))
+        bbox_to_anchor=(0.15, 0.,0.65, -0.5))
+
 
 
 def show_summary(progLang: np.ndarray,
@@ -39,9 +43,9 @@ def show_summary(progLang: np.ndarray,
     Draws and displays visuals, within the same figure
     '''
     plt.subplot(1,2,1)
-    draw_bar_graph(progLang, frequencyArr)
+    patches, labels = draw_pie_charts(progLang, frequencyArr)
     plt.subplot(1,2,2)
-    draw_pie_charts(progLang, frequencyArr)
+    draw_pie_chart_legend(patches, labels)
     plt.show()
 
 def main():
@@ -54,7 +58,7 @@ def main():
             else:
                 companyDict[row[1]] += 1
     for company in list(companyDict.keys()):
-        if companyDict[company] <= 2:
+        if companyDict[company] == 1:
             companyDict.pop(company)
 
     show_summary(list(companyDict.keys()), np.array(list(companyDict.values())))
